ac6b0750fddb41be05227ad171ec559e816df6ca
Ram Kishor <krishnaenterprises0423@gmail.com>
2024-02-05 00:23:07 +0530

    ok

--- src/main/java/com/brijframework/server/ServerApplication.java
+++ src/main/java/com/brijframework/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.brijframework.cloudserver;
+package com.brijframework.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class CloudserverApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudserverApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
